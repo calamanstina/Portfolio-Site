ef55b7dd732beffc0626904ac6ab6dfa2045ecba
calamanstina <christinaneri.essink@gmail.com>
2022-11-25 13:03:39 +0100

    fix js attribute

--- js/script.js
+++ js/script.js
@@ -1,10 +1,1 @@
 console.log("Hello world");
-var age = 28;
-var name = "christina";
-var hobbies = "reading and learning new things"
-var person= {
-  name: name,
-  age: age,
-  hobbies: hobbies,
-};
-console.log(person);
