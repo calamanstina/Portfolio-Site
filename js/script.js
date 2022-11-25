e45abe29b3983769f79c78a1a0d2ae4453c3af06
calamanstina <christinaneri.essink@gmail.com>
2022-11-25 16:51:43 +0100

    fix nav toggle code

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 console.log("Hello world");
 
 function menuToggle() {
-  var x = document.getElementbyId('myNavtoggle');
+  var x = document.getElementById('myNavtoggle');
   if (x.className === 'navtoggle') {
-    x.className += ' responsive ';
-  } else { 
+    x.className += ' responsive';
+  } else {
     x.className = 'navtoggle';
   }
 }
