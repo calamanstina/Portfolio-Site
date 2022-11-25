4956d28ecfed71917c44c5a0f44900fb962096d5
calamanstina <christinaneri.essink@gmail.com>
2022-11-25 16:44:02 +0100

    add menu responsive functions

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 console.log("Hello world");
+
+function menuToggle() {
+  var x = document.getElementbyId('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive ';
+  } else { 
+    x.className = 'navtoggle';
+  }
+}
